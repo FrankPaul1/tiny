d6e96e26fb7a7c78fc6b27ca6191666bc92f9560
zoushicheng <zoushicheng@bytedance.com>
2018-11-04 23:18:14 +0800

    chore: add test demo
    Change-Id: Id1d85473add7bacba1609c117b5724930a071863

--- template-engines/ejs/test/index.test.js
+++ template-engines/ejs/test/index.test.js
@@ -1,6 +1,7 @@
+const { test } = require('../index');
 
 describe('[ejs]: test for jest', () => {
-  it('sum', () => {
-    expect(1 + 2).toBe(3)
+  it('test', () => {
+    expect(test()).toBe('test')
   })
 })
